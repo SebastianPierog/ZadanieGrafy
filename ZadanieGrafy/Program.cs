18a6525697fb4e9b2747a97219f5478e8938747a
Pan Ktos <lambda19935@gmail.com>
2021-02-09 18:10:31 +0100

    Dodaj pliki projektów.

---
+++ ZadanieGrafy/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZadanieGrafy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            if (args.Length == 1)
+            {
+                var tree = new SuffixTree();
+                var text = tree.readFile(args[0]);
+                tree.AddString(text);
+                tree.znajdzK(text);
+            }
+        }
+    }
+}
